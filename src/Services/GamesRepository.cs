617c93c3a2b2b916d3b43043eb866da7d9f8acf1
Maxim <dddg.dddfgg@yandex.ru>
2022-04-16 13:23:10 +0500

    added IsEmptyForObject

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using thegame.Models;
 
@@ -16,59 +14,69 @@ namespace thegame.Services
             var height = 9;
             var cells = new CellDto[width * height + 2];
 
-            for (int i = 0; i < width; i++)
+            for (var i = 0; i < width; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (var j = 0; j < height; j++)
                 {
-                    cells[j + i * height] = new CellDto((i * j).ToString(), new VectorDto(i, j), "b", "", 0);
+                    cells[j + i * height] = new CellDto((j + i * height).ToString(), new VectorDto(i, j), "b", "", 0);
                 }
             }
             
-            for (int i = 1; i < width - 1; i++)
+            for (var i = 1; i < width - 1; i++)
             {
-                for (int j = 1; j < height - 1; j++)
+                for (var j = 1; j < height - 1; j++)
                 {
-                    cells[j + i * height] = new CellDto((i * j).ToString(), new VectorDto(i, j), "b2", "", 0);
+                    cells[j + i * height] = new CellDto((j + i * height).ToString() + "sdsd", new VectorDto(i, j), "b2", "", 2);
                 }
             }
             
-            cells[^1] = new CellDto("User", new VectorDto(5, 5), "u", "", 0);
+            cells[^1] = new CellDto("User", new VectorDto(5, 5), "u", "", 2);
             cells[^2] = new CellDto("Point", new VectorDto(6, 6), "b1", "*", 1);
             
             return CurrentGame = new GameDto(null, cells, true, true, width, height, Guid.Empty, false, 0);
         }
 
-        public bool IsEmptyForObject(string objTag, VectorDto position)
+        public static bool IsEmptyForObject(string objTag, VectorDto position)
+        {
+           return CurrentGame.Cells
+               .Where(x => x.Pos.Equals(position))
+               .FirstOrDefault(x => x.ZIndex == CurrentGame.Cells[FindIndexByTag(objTag)].ZIndex) is null;
+        }
+
+        public static bool IsFinished()
         {
             throw new NotImplementedException();
         }
 
-        public bool IsFinished()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryPushObject(string pusherTag, VectorDto pusherDelta)
+        public static bool TryPushObject(string pusherTag, VectorDto pusherDelta)
         {
             throw new NotImplementedException();
         }
 
         public static GameDto MoveObjOnDelta(string objTag, VectorDto delta)
         {
-            var index = CurrentGame.Cells.Select((x, i) => (x, i)).FirstOrDefault(x => x.x.Id == objTag).i;
-            var movedVector = new VectorDto(delta.X + CurrentGame.Cells[index].Pos.X,
-                delta.Y + CurrentGame.Cells[index].Pos.Y);
-
+            var index = FindIndexByTag(objTag);
+            var objPos = CurrentGame.Cells[index].Pos;
+            var movedVector = new VectorDto(delta.X + objPos.X, delta.Y + objPos.Y);
+            
             return SetNewVectorFor(objTag, movedVector);
         }
 
         public static GameDto SetNewVectorFor(string objTag, VectorDto to)
         {
-            var index = CurrentGame.Cells.Select((x, i) => (x, i)).FirstOrDefault(x => x.x.Id == objTag).i;
+            if (IsEmptyForObject(objTag, to)) 
+                return CurrentGame;
             
-            CurrentGame.Cells[index] = new CellDto(objTag, to, CurrentGame.Cells[index].Type, CurrentGame.Cells[index].Content, CurrentGame.Cells[index].ZIndex);
+            var index = FindIndexByTag(objTag);
+            var obj = CurrentGame.Cells[index];
+            CurrentGame.Cells[index] = new CellDto(objTag, to, obj.Type, obj.Content, obj.ZIndex);
             
             return CurrentGame = new GameDto(null, CurrentGame.Cells, true, true, CurrentGame.Width, CurrentGame.Height, CurrentGame.Id, false, 0);
+        }
+
+        private static int FindIndexByTag(string tag)
+        {
+            return CurrentGame.Cells.Select((x, i) => (x, i)).FirstOrDefault(x => x.x.Id == tag).i;
         }
     }
 }

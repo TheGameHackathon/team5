a153f83c0d710f81d46a8459d17d36543501477f
Евгений <evgeniisergeev2002@list.ru>
2022-07-04 11:17:54 +0500

    add CreateField method

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,5 +1,49 @@
+using System;
+using thegame.Models;
+
 namespace thegame.Services;
 
 public class GamesRepository
 {
+    public GameDto CreateField(Difficult dif)
+    {
+        var width = 8;
+        var height = 8;
+        var colorAmount = 3;
+
+        switch (dif)
+        {
+            case Difficult.Eazy:
+                width = height = 8;
+                colorAmount = 3;
+                break;
+            case Difficult.Medium:
+                width = height = 21;
+                colorAmount = 5;
+                break;
+            case Difficult.Hard:
+                width = height = 55;
+                colorAmount = 8;
+                break;
+        }
+
+        var testCells = new CellDto[width * height];
+
+        for (var j = 0; j < width; j++)
+        {
+            for (var i = 0; i < height; i++)
+            {
+                var index = j * width + i;
+
+                var colorIndex = new Random().Next(1, colorAmount + 1);
+                var color = $"color{colorIndex}";
+
+                var cell = new CellDto(index.ToString(),
+                                        new VectorDto { X = i, Y = j }, color, "", 0);
+
+                testCells[index] = cell;
+            }
+        }
+        return new GameDto(testCells, true, true, width, height, Guid.Empty, false, 0);
+    }
 }
---
+++ src/Services/IFieldGenerator.cs
@@ -0,0 +1,10 @@
+﻿using thegame.Models;
+using static thegame.Services.FieldGenerator;
+
+namespace thegame.Services
+{
+    public interface IFieldGenerator
+    {
+        public GameDto Create(Difficult dif);
+    }
+}

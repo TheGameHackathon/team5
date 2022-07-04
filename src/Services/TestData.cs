24a330cf55aedcc4cf21c275c58cd897abbd205b
Евгений <evgeniisergeev2002@list.ru>
2022-07-04 10:59:40 +0500

    add field generator

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -7,16 +7,29 @@ public class TestData
 {
     public static GameDto AGameDto(VectorDto movingObjectPosition)
     {
-        var width = 5;
-        var height = 5;
-        var testCells = new[]
+        var width = 8;
+        var height = 8;
+        var colorAmount = 3;
+        //8*8 3
+        //21*21 5
+        //55*55 8
+        var testCells = new CellDto[width * height];
+
+        for (var j = 0; j < width; j++)
         {
-            new CellDto("1", new VectorDto {X = 2, Y = 4}, "color1", "", 0),
-            new CellDto("2", new VectorDto {X = 2, Y = 4}, "color1", "", 0),
-            new CellDto("3", new VectorDto {X = 3, Y = 1}, "color2", "", 20),
-            new CellDto("4", new VectorDto {X = 1, Y = 0}, "color2", "", 20),
-            new CellDto("5", movingObjectPosition, "color4", "☺", 10),
-        };
+            for (var i = 0; i < height; i++)
+            {
+                var index = j * width + i;
+
+                var colorIndex = new Random().Next(1, colorAmount+1);
+                var color = $"color{colorIndex}";
+
+                var cell = new CellDto(index.ToString(),
+                                        new VectorDto { X = i, Y = j }, color, "", 0);
+                testCells[index] = cell;
+            }
+        }
+
         return new GameDto(testCells, true, true, width, height, Guid.Empty, movingObjectPosition.X == 0, movingObjectPosition.Y);
     }
 }
